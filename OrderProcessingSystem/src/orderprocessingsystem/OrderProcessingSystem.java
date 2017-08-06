7fcb7fba3abd0f9ab6d71acb21edb7b6b6d8904a
Rishav Gupta <gupta.rishav2608@gmail.com>
2017-08-06 18:38:02 +0530

    Lab1 8 functionaties added

--- OrderProcessingSystem/src/orderprocessingsystem/OrderProcessingSystem.java
+++ OrderProcessingSystem/src/orderprocessingsystem/OrderProcessingSystem.java
@@ -14,7 +14,8 @@ public class OrderProcessingSystem {
         ArrayList<Integer> quantity=new ArrayList<>();
         int price=0;
         while(true){
-            System.out.println("1.Login \t 2.Register \t 3.Exit");
+            int f1=0;
+            System.out.println("1.Login \t 2.Register \t 3.Admin Login\t 4.Exit");
             int choice=sc.nextInt();
             switch(choice)
             {
@@ -39,6 +40,9 @@ public class OrderProcessingSystem {
                         while(true){
                             System.out.println("Items to buy");
                             System.out.println("1. Monitor-Rs.1000\t2. CPU-Rs.1500\t3. Keyboard-Rs.300\t4. Mouse-Rs.200");
+                            System.out.println("What do you want to do:\n1.Item buying\t2.Change Password\t3.Logout");
+                            int ch1=sc.nextInt();
+                            if(ch1==1){
                             System.out.println("Enter item number you want to buy: ");
                             items.add(sc.nextInt());
                             System.out.println("Enter quantity: ");
@@ -90,7 +94,7 @@ public class OrderProcessingSystem {
                                     for(long i=0;i<1000000000;i++);
                                     for(long i=0;i<1000000000;i++);
                                     System.out.println("Your order has been delivered! Thank you for shopping with us!");
-                                    break;
+                                    
                                 }
                                 else{
                                     System.out.println("Do you want to add more items?(y/n)");
@@ -99,6 +103,38 @@ public class OrderProcessingSystem {
                                         continue;
                                     }
                                 }
+                                
+                            }
+                            }
+                            else if(ch1==2)
+                            {
+                                System.out.println("Enter old password:");
+                                String oldpwd=sc.next();
+                                System.out.println("Enter new password:");
+                                String newpwd=sc.next();
+                                Iterator it=password.iterator();
+                                int value=-1;
+                                int flag1=0;
+                                while(it.hasNext()){
+                                value++;
+                                String n1=(String)it.next();
+                                if(n1.equals(oldpwd))
+                                {
+                                    flag1=1;
+                                    break;
+                                }
+                            }
+                            if(flag1==1){
+                                password.set(value,newpwd);
+                                System.out.println("Password changed successfully!");
+                            }
+                            else
+                            {
+                                System.out.println("Enter correct password!");
+                            }
+                            }
+                            else
+                            {
                                 break;
                             }
                         }
@@ -123,10 +159,75 @@ public class OrderProcessingSystem {
                     password.add(p1);
                     System.out.println("Sending you to LOGIN page!");
                     continue;
+                case 3:
+                    System.out.println("Enter Name: ");
+                    String n3=sc.next();
+                    System.out.println("Enter Password: ");
+                    String p3=sc.next();
+                    if(n3.equals("rishav")&& p3.equals("rishav")){
+                        while(true){
+                    System.out.println("1.Show Users\t2.Show Last Order\t3.Logout");
+                    int ch2=sc.nextInt();
+                    if(ch2==1)
+                    {
+                        Iterator iterator=name.iterator();
+                        String s="";
+                        while(iterator.hasNext())
+                        { 
+                            s+=iterator.next()+"\n";
+                        }
+                        if(!s.equals(""))
+                            System.out.println(s);
+                        else
+                            System.out.println("No users till now");
+                    }
+                    else if(ch2==2)
+                    {
+                        Iterator iterator=items.iterator();
+                        Iterator iterator1=quantity.iterator();
+                        String s="";
+                        while(iterator.hasNext())
+                        {
+                            switch((int)iterator.next())
+                            {
+                                case 1:
+                                s+="Monitor";
+                                break;
+                                case 2:
+                                s+="CPU";
+                                break;
+                                case 3:
+                                s+="Keyboard";
+                                break;
+                                case 4:
+                                s+="Mouse";
+                                break;
+                            }
+                            s+="  "+iterator1.next()+ " quantity"+"\n";
+                        }
+                        if(!s.equals(""))
+                            System.out.println(s);
+                        else
+                            System.out.println("No orders till now!");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                        }
+                    }
+                    else
+                    {
+                        System.out.println("Invalid Credentials");
+                    }
+                    break;
                 default:
                     System.out.println("Exiting!!!");
+                    f1=1;
             }
-            break;
+            if(f1==1)
+                break;
+            
         }
     }
 }
